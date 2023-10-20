f902b53eb3ef3f2757d940744a8e3cdf6496711e
Juan Parra <haden9@gmail.com>
2023-10-20 01:05:14 -0400

    add terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "jp"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "eastus"
+}
